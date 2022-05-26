ade2f6a5b2ab6c321cbeec1ede8637c70946bd00
JonathanBacklin <Jonathan.backlin@gmail.com>
2022-05-26 02:10:31 +0200

    Github Pages

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './Pages/App';
 import Discover from './Pages/Discover';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import ClickedShell from './Pages/ClickedShell';
 import Footer from './ReusableComponents/Footer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/Discover" element={<Discover />} />
@@ -18,7 +18,7 @@ ReactDOM.render(
         <Route path="*" element={<App />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
